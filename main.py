02eba5736c4448cadb92ceae801947af922f878b
Gabriel Locher <grlocher@gmail.com>
2024-05-01 21:24:30 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+exercise1 = input('Enter some text: << ')
+print(exercise1)
+
+exercise2 = input('Enter a number ')
+exercise2 = int(exercise2)
+print(exercise2 + 1)
+
+exercise3 = input('Enter a number ')
+exercise3 = float(exercise3)
+print(exercise3 + .5)
+
+exercise4 = float(input('Enter a number '))
+exercise_4 = float(input('Enter another number '))
+print('The sum is', exercise4 + exercise_4)
+
+exercise5 = float(input('Enter a number '))
+exercise_5 = float(input('Enter another number '))
+print('The product is', exercise5 * exercise_5)
+
+exercise6 = int(input('Enter a number '))
+exercise_6 = int(input('Enter another number '))
+print('The result is', exercise6 / exercise_6)
